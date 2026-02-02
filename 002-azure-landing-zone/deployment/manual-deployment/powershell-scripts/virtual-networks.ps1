0433188891a0e186bf544376f43d517df5af0b0c
smulbero <smulbero@gmail.com>
2026-02-02 19:06:09 +0200

    Functionality to create hub and spoke network peerings added

--- 002-azure-landing-zone/deployment/manual-deployment/powershell-scripts/virtual-networks.ps1
+++ 002-azure-landing-zone/deployment/manual-deployment/powershell-scripts/virtual-networks.ps1
@@ -1,63 +1,5 @@
-$RESOURCE_VNET_PREFIX = "vnet"
-
 # -------------------------------
-# Helper funtions start
-# -------------------------------
-
-function operationOutput {
-    param ( 
-        [Parameter(Mandatory = $true)]
-        $operation,
-        $resource
-    )
-    switch ($operation) {
-        "SKIPPED_FOUND"     { Write-Host "[SKIPPED] Virtual network '$resource' already exists"     -ForegroundColor Yellow }
-        "SKIPPED_NOT_FOUND" { Write-Host "[SKIPPED] Virtual network '$resource' doesn't exists"     -ForegroundColor Yellow }
-        "CREATE_FAILED"     { Write-Host "[FAILED] Failed to create virtual network: $resource"     -ForegroundColor Red    }
-        "DELETE_FAILED"     { Write-Host "[FAILED] Failed to delete virtual network: $resource"     -ForegroundColor Red    }
-        "CREATE_SUCCEED"    { Write-Host "[SUCCESS] Created virtual network: $resource"             -ForegroundColor Green  }
-        "DELETE_SUCCEED"    { Write-Host "[SUCCESS] Deleted virtual network: $resource"             -ForegroundColor Green  }
-    }    
-}
-
-# Returns object with name and location attributes
-function getResourceGroupInfo {
-    param (
-        [Parameter(Mandatory = $true)]
-        $name
-    )
-        
-    $resource_group = az group list --query "[?contains(name, '$name')]" | ConvertFrom-Json
-        
-    return $resource_group
-}
-
-# Return network name
-function getHubNetworkInfo {
-    param (
-        [Parameter(Mandatory = $true)]
-        $name
-    )
-
-    $hub_network_name = az network vnet list --query "[?contains(name, '$name')]" | ConvertFrom-Json
-
-    return $hub_network_name
-}
-
-# Return network name
-function getSpokeNetworkInfo {
-    param (
-        [Parameter(Mandatory = $true)]
-        $name
-    )
-    
-    $spoke_network_name = az network vnet list --query "[?contains(name, '$name')]" | ConvertFrom-Json
-
-    return $spoke_network_name
-}
-
-# -------------------------------
-# Helper funtions end
+# Core functions
 # -------------------------------
 
 # Create virtual networks
@@ -79,66 +21,70 @@ function createVirtualNetworks {
     $skipped = 0
     $failed = 0    
 
-    Write-Output "# --------------------"
-    Write-Output "# Creating virtual networks.."
-    Write-Output "# --------------------"
+    Write-Host "# --------------------"
+    Write-Host "# Creating virtual networks.."
+    Write-Host "# --------------------"
     foreach ($d in $data) {
-        $resource_group = getResourceGroupInfo -name $d.name
-        $resource_name = "$($RESOURCE_VNET_PREFIX)-$($d.name)-$($resource_group.location)"
-
-        # Check if resource already exists
-        az network vnet show `
-            --name $resource_name `
-            --resource-group $resource_group.name `
-            --output none 2>$null
-
-        if ($LASTEXITCODE -eq 0) {
-            operationOutput -operation "SKIPPED_FOUND" -resource $resource_name
+        # Check if resource exists
+        $existing_resource = (getNetworkInfo -name $d.name)
+        if ($existing_resource.id) {
+            operationOutput -operation "SKIPPED" -message "Virtual network '$($existing_resource.name)' already exists"
             $skipped++
             continue
         }
+
+        # Check if resource group for vnet exists
+        $resource_group = getResourceGroupInfo -name $d.name
+        if (!$resource_group.id) {
+            operationOutput -operation "SKIPPED" -message "Resource group for '$($d.name)' not found"
+            $skipped++
+            continue
+        }
+
+        $network_name = "$VNET_PREFIX-$($d.name)-$($resource_group.location)"
 
         if ($d.tags -and $d.tags.Count -gt 0) {
             # Create key=value tag pairs
             $tagsParam = @(
                 $d.tags.GetEnumerator() | ForEach-Object { $_.Key, $_.Value -join '=' }
             )
-    
+            
+            # Try to create new vnet
             az network vnet create `
-                --name $resource_name `
+                --name $network_name `
                 --resource-group $resource_group.name `
+                --address-prefix $d.address_prefix `
                 --tags $tagsParam `
                 --no-wait `
                 --output none 2>&1 | Tee-Object -FilePath "logs/vnet-create-error.log" -Append | Out-Null
         }
         else {
-            # Attempt to create resource without tags
-            # Shouldn't happen in this project since required tags are enforced
+            # Try to create new vnet without tags
             az network vnet create `
-                --name $resource_name `
+                --name $network_name `
                 --resource-group $resource_group.name `
+                --address-prefix $d.address_prefix `
                 --output none 2>&1 | Tee-Object -FilePath "logs/vnet-create-error.log" -Append | Out-Null
         }
 
         if ($LASTEXITCODE -ne 0) {
-            operationOutput -operation "CREATE_FAILED" -resource $resource_name
+            operationOutput -operation "FAILED" -message "Failed to create virtual network: $network_name"
             $failed++
         }
         else {
-            operationOutput -operation "CREATE_SUCCEED" -resource $resource_name
+            operationOutput -operation "SUCCEED" -message "Created virtual network: $network_name"
             $created++
         }
     }
-    Write-Output "# --------------------"
-    Write-Output "# ..virtual networks created"
-    Write-Output "# --------------------"
-    Write-Output ""
-    Write-Output "# --------------------"
-    Write-Output "# Summary"
-    Write-Output "# --------------------"    
-    Write-Output "Created: $created"    
-    Write-Output "Skipped: $skipped"    
-    Write-Output "Failed: $failed"    
+    Write-Host "# --------------------"
+    Write-Host "# ..virtual networks created"
+    Write-Host "# --------------------`n"
+    Write-Host "# --------------------"
+    Write-Host "# Summary"
+    Write-Host "# --------------------"    
+    Write-Host "Created: $created"    
+    Write-Host "Skipped: $skipped"    
+    Write-Host "Failed: $failed`n"    
 }
 
 # Delete virtual networks
@@ -163,160 +109,154 @@ function deleteVirtualNetworks {
     $confirmation = (Read-Host "Delete given virtual networks? (y/n)").ToLower()
 
     if ($confirmation -eq "n") {
-        Write-Output "Aborting virtual network deletion"
+        Write-Host "Aborting virtual network deletion"
         return
     }
-    Write-Output "# --------------------"
-    Write-Output "# Deleting virtual networks.."
-    Write-Output "# --------------------"
+    Write-Host "# --------------------"
+    Write-Host "# Deleting virtual networks.."
+    Write-Host "# --------------------"
     foreach ($d in $data) {
-        $resource_group = az group list --query "[?contains(name, '$($d.name)')].{name:name, location:location}" |  Tee-Object -filepath ConvertFrom-Json
-        $resource_name = "$($RESOURCE_VNET_PREFIX)-$($d.name)-$($resource_group.location)"
-        
         # Check if resource exists
-        az network vnet show `
-            --name $resource_name `
-            --resource-group $resource_group.name `
-            --output none 2$null
-
-        if ($LASTEXITCODE -ne 0) {
-            operationOutput -operation "SKIPPED_NOT_FOUND" -resource $resource_name
+        $existing_resource = (getNetworkInfo -name $d.name)       
+        if (!$existing_resource.id) {
+            operationOutput -operation "SKIPPED" -message "Virtual network '$($d.name)' doesn't exist"
             $skipped++
             continue
         }
 
+        # Try to delete resource
         az network vnet delete `
-            --resource-group $resource_group.name `
-            --name $resource_name `
+            --resource-group $existing_resource.resourceGroup `
+            --name $existing_resource.name `
             --no-wait `
-            --yes `
             --output none 2>&1 | Tee-Object -FilePath "logs/vnet-delete-error.log" -Append | Out-Null
 
         if ($LASTEXITCODE -ne 0) {
-            operationOutput -operation "DELETE_FAILED" -resource $resource_name
+            operationOutput -operation "FAILED" -message "Failed to delete virtual network: $($existing_resource.name)"
             $failed++
         }
         else {
-            operationOutput -operation "DELETE_SUCCEED" -resource $resource_name
+            operationOutput -operation "SUCCEED" -message "Deleted virtual network: $($existing_resource.name)"
             $deleted++
         }
     }
-    Write-Output "# --------------------"
-    Write-Output "# ..virtual networks deleted"
-    Write-Output "# --------------------"
-    Write-Output ""
-    Write-Output "# --------------------"
-    Write-Output "# Summary"
-    Write-Output "# --------------------"    
-    Write-Output "Deleted: $deleted"    
-    Write-Output "Skipped: $skipped"    
-    Write-Output "Failed: $failed"    
+    Write-Host "# --------------------"
+    Write-Host "# ..virtual networks deleted"
+    Write-Host "# --------------------`n"
+    Write-Host "# --------------------"
+    Write-Host "# Summary"
+    Write-Host "# --------------------"    
+    Write-Host "Deleted: $deleted"    
+    Write-Host "Skipped: $skipped"    
+    Write-Host "Failed: $failed`n"    
 }
 
 # Create network peerings
-# -------------------------------
-# --remote-vnet would need a very specific value where to find the right resources
-# could be done, but I rather not for this project
-# -------------------------------
-# function createHubAndSpoke {
-#     param (
-#         [Parameter(Mandatory = $true)]
-#         [ValidateScript({
-#                 foreach ($d in $_) {
-#                     if (!$d.name -or !$d.network) {
-#                         throw "Given data must contain following properties: 'name', 'network"
-#                     }
-#                 }
-#                 return $true
-#             })]
-#         $data
-#     )
+function createHubAndSpoke {
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({
+                foreach ($d in $_) {
+                    if (!$d.name -or !$d.network) {
+                        throw "Given data must contain following properties: 'name', 'network"
+                    }
+                }
+                return $true
+            })]
+        $data
+    )
 
-#     $created = 0
-#     $failed = 0
+    $created = 0
+    $failed = 0
+    $skipped = 0
 
-#     Write-Output "# --------------------"
-#     Write-Output "# Peering vnets.."
-#     Write-Output "# --------------------"
+    $hub_network = $null
+    $spoke_network = $null
 
-#     $hub_network_name = $null
-#     $hub_resource_group = $null
+    Write-Host "# --------------------"
+    Write-Host "# Peering vnets.."
+    Write-Host "# --------------------"
 
-#     # Get hub vnet name
-#     foreach ($d in $data) {
-#         if ($d.network.ToLower() -eq "hub") {
-#             $hub_network = getHubNetworkInfo -name $d.name
-#             $hub_network_name = $hub_network.name
-#             $hub_resource_group = $hub_network.resourceGroup
-#             break
-#         }
-#         else {
-#             Write-Output "Network for hub assignment not found, exiting"
-#             return
-#         }
-#     }
+    # Get a vnet with hub assignment, break the loop on first match
+    foreach ($d in $data) {
+        if ($d.network.ToLower() -eq "hub") {
+            $hub_network = getNetworkInfo -name $d.name
+            break
+        }
+        else {
+            operationOutput -operation "FAILED" -message "Network for hub assignment not found, exiting"
+            return
+        }
+    }
 
-#     # Create peerings
-#     foreach ($d in $data) {
-#         if ($d.network.ToLower() -eq "hub" -or $d.network -eq "") {
-#             continue
-#         }        
+    # Create peerings
+    foreach ($d in $data) {
+        # Find spoke networks from data
+        if ($d.network.ToLower() -eq "hub" -or $d.network -eq "") {
+            continue
+        }        
 
-#         # Create peering from hub to spoke
-#         $spoke_network = getSpokeNetworkInfo -name $d.name
-#         $spoke_network_name = $spoke_network.name
-#         $peering_name = "$hub_network_name-$spoke_network_name"
+        $spoke_network = getNetworkInfo -name $d.name
+        # Check if spoke network exists
+        if(!$spoke_network.id) {
+            operationOutput -operation "SKIPPED" -message "Spoke network for '$($d.name)' not found"
+            $skipped++
+            continue
+        }
 
-#         az network vnet peering create `
-#             --name $peering_name `
-#             --resource-group $hub_resource_group `
-#             --vnet-name $hub_network_name `
-#             --remote-vnet $spoke_network_name `
-#             --allow-forwarded-traffic true `
-#             --allow-vnet-access true `
-#             --no-wait `
-#             --output none 2>&1 | Tee-Object -FilePath "logs/peering-error.log" -Append | Out-Null
+        # Create peering from hub to spoke
+        $peering_name = "$($hub_network.name)-$($spoke_network.name)"
 
-#         if ($LASTEXITCODE -ne 0) {
-#             Write-Output "[FAILED] Failed to create a vnet peering from '$hub_network_name' to '$spoke_network_name'"
-#             $failed++
-#             continue
-#         }
-#         else {
-#             Write-Output "[SUCCEED] Created a vnet peering from '$hub_network_name' to '$spoke_network_name'"
+        az network vnet peering create `
+            --name $peering_name `
+            --resource-group $hub_network.resourceGroup `
+            --vnet-name $hub_network.name `
+            --remote-vnet $spoke_network.id `
+            --allow-forwarded-traffic true `
+            --allow-vnet-access true `
+            --no-wait `
+            --output none 2>&1 | Tee-Object -FilePath "logs/peering-error.log" -Append | Out-Null
 
-#             # Create peering from spoke to hub
-#             $peering_name = "$spoke_network_name-$hub_network_name"
+        if ($LASTEXITCODE -ne 0) {
+            operationOutput -operation "FAILED" -message "Failed to create a vnet peering from '$($hub_network.name)' to '$($spoke_network.name)'"
+            $failed++
+            continue
+        }
+        else {
+            operationOutput -operation "SUCCEED" -message "Created a vnet peering from '$($hub_network.name)' to '$($spoke_network.name)'"
 
-#             az network vnet peering create `
-#             --name $peering_name `
-#             --resource-group $hub_resource_group `
-#             --vnet-name $spoke_network_name `
-#             --remote-vnet $hub_network_name `
-#             --allow-forwarded-traffic true `
-#             --allow-vnet-access true `
-#             --no-wait `
-#             --output none 2>&1 | Tee-Object -FilePath "logs/peering-error.log" -Append | Out-Null
+            # Create peering from spoke to hub
+            $peering_name = "$($spoke_network.name)-$($hub_network.name)"
 
-#             if ($LASTEXITCODE -ne 0) {
-#                 Write-Output "[FAILED] Failed to create a vnet peering from '$spoke_network_name' to '$hub_network_name'"
-#                 $failed++
-#                 continue
-#             }
-#             else {
-#                 Write-Output "[SUCCEED] Created a vnet peering from '$spoke_network_name' to '$hub_network_name'"
-#                 $created++
-#             }
-#         }
-#     }
+            az network vnet peering create `
+            --name $peering_name `
+            --resource-group $spoke_network.resourceGroup `
+            --vnet-name $spoke_network.name `
+            --remote-vnet $hub_network.id `
+            --allow-forwarded-traffic true `
+            --allow-vnet-access true `
+            --no-wait `
+            --output none 2>&1 | Tee-Object -FilePath "logs/peering-error.log" -Append | Out-Null
 
-#     Write-Output "# --------------------"
-#     Write-Output "# ..vnet peering done"
-#     Write-Output "# --------------------"
-#     Write-Output ""
-#     Write-Output "# --------------------"
-#     Write-Output "# Summary"
-#     Write-Output "# --------------------"    
-#     Write-Output "Created: $created"    
-#     Write-Output "Failed: $failed" 
-# }
+            if ($LASTEXITCODE -ne 0) {
+                operationOutput -operation "FAILED" -message "Failed to create a vnet peering from '$($spoke_network.name)' to '$($hub_network.name)'"
+                $failed++
+                continue
+            }
+            else {
+                operationOutput -operation "SUCCEED" -message "Created a vnet peering from '$($spoke_network.name)' to '$($hub_network.name)'"
+                $created++
+            }
+        }
+    }
+
+    Write-Host "# --------------------"
+    Write-Host "# ..vnet peering done"
+    Write-Host "# --------------------`n"
+    Write-Host "# --------------------"
+    Write-Host "# Summary"
+    Write-Host "# --------------------"    
+    Write-Host "Created: $created"    
+    Write-Host "Skipped: $skipped"    
+    Write-Host "Failed: $failed`n" 
+}
